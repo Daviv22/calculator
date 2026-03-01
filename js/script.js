925f5ec806b01e1efe76427921511147d7997efc
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-03-01 18:41:27 -0300

    Adiciona funcionalidade nas operações, mas falta adicionar um mecanismo para diferenciar a primeira expressão da segunda

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
 const operandos = document.querySelectorAll('.operandos');
 const operadores = document.querySelectorAll('.operadores');
+const equals = document.getElementById('equals');
+
+
+let firstNumber = 0;
+let lastNumber = 0;
+let operator = "";
+let result = 0;
+
+function calculator(a, b, operation) {
+    console.log(operation)
+    if (operation === '+') {
+        return a + b;
+    } else if (operation === '-') {
+        return a - b;
+    } else if (operation === '*') {
+        return a * b;
+    } else if (operation === '/') {
+        return a / b;
+    }
+}
 
 operandos.forEach(operando => {
     operando.addEventListener('click', function () {
-        console.log(this.value)
+        firstNumber = Number(this.value);
+        console.log(`first ${firstNumber}`);
+    })
+})
+
+operandos.forEach(operando => {
+    operando.addEventListener('click', function () {
+        lastNumber = Number(this.value);
+        console.log(`last ${lastNumber}`);
     })
 })
 
 operadores.forEach(operador => {
     operador.addEventListener('click', function () {
-        console.log(this.value)
+        operator = this.value;
     })
+})
+
+equals.addEventListener('click', function () {
+    result = calculator(firstNumber, lastNumber, operator);
+    console.log(result);
 })
